14762fe7bd0d80fe940f42ceae24fdf55a7f6ac8
LyesOCDevSF <fourcroy.l@outlook.fr>
2022-09-15 01:39:41 +0200

    INIT

---
+++ Nouveau dossier/index.js
@@ -0,0 +1,11 @@
+// ici on prépare la recherche par déclenchement du bouton recherche//
+function buttonSearchGet(){
+    //on met l'url dans une variable//
+    var apiGoogle = "https://www.googleapis.com/books/v1/{collectionName}/resourceID?parameters";
+    var apiGoogle = "https://www.googleapis.com/books/v1/{collectionName}/resourceID?parameters";
+    
+    let book = {
+        title : "",
+        collectionName :"",
+    };
+}
