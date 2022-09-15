c93d26c3fe29af06c138ff2c351b05929661f76a
LyesOCDevSF <fourcroy.l@outlook.fr>
2022-09-15 02:02:20 +0200

    ajout bouton recherche et barre de recherche#essai

--- Nouveau dossier/index.js
+++ Nouveau dossier/index.js
@@ -1,11 +1,15 @@
+
+
+document.getElementById("button").onclick = buttonSearchGet;
+
+Source: https://prograide.com/pregunta/17839/utiliser-un-bouton-html-pour-appeler-une-fonction-javascript
+
 // ici on prépare la recherche par déclenchement du bouton recherche//
+
+
+
 function buttonSearchGet(){
     //on met l'url dans une variable//
     var apiGoogle = "https://www.googleapis.com/books/v1/{collectionName}/resourceID?parameters";
-    var apiGoogle = "https://www.googleapis.com/books/v1/{collectionName}/resourceID?parameters";
-    
-    let book = {
-        title : "",
-        collectionName :"",
-    };
+    goto (apiGoogle);
 }
