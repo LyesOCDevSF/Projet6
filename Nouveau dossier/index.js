146e10fb69ab57e541e6bd9be5081c7d8af4591d
LyesOCDevSF <fourcroy.l@outlook.fr>
2022-12-09 12:13:02 +0100

    ajout div caché

--- Nouveau dossier/index.js
+++ Nouveau dossier/index.js
@@ -1,44 +1,12 @@
 
-// ------------------------------------- ESSAI FORMULAIRE DYNAMIQUE ----------------------------------
-
-/*const buttonElement = document.createElement("button");
-const sectionBouton = doucment.querySelector(".btn");
-sectionBouton.appendChild(buttonElement);*/
 
 
-/*let boutonElement = document.getElementById("Bouton");
-boutonElement.addEventListener("click", addBooks);*/
-
-// ajouter un formulaire en js avec une fonction ?
-// le formulaire s'affiche au niveau du click
-//creer le bouton innerhtml
-
-
-
-
-//let divformulaire = document.getElementById("divFormulaire")
-//divformulaire.style.display = 'none';
-
-/*function addBooks(){
-    //divformulaire = document.getElementById("divFormulaire")
-    divformulaire.style.display= 'block';
-    console.log(divformulaire);*/
-
-
-document.getElementById(".champ_cache").style.display = "none";
+ let divHide = document.getElementById("div1");
+ divHide.style.display="none";
 function addBooks(){
-document.getElementById(".champ_cache").style.display = "block";
+document.getElementById("div1").style.display = "block";
 }
-// ---------------------------------------------- ESSAI AJOUT ELEMENT -------------------------------
-//function addElement(){
-   // let newDiv = document.createElement('div');
-   // let newForm = document.createElement('form');
-    //let newInput = document.createElement('input');
 
-    let elt = document.getElementById('myBooks');
-    elt.innerHTML = "<form></form>";
-    //console.log("le formulaire est là");
-// ajout des elements avec innerHtml et textcontent
 
 
 function test(){
