00a774dcf05d4b3d3c53c00674680eab274de348
LyesOCDevSF <fourcroy.l@outlook.fr>
2022-12-18 11:42:17 +0100

    diplay section with js

--- Nouveau dossier/index.js
+++ Nouveau dossier/index.js
@@ -1,45 +1,62 @@
 
 
-
- let divHide = document.getElementById("div1");
- divHide.style.display="none";
- let searchResult = document.getElementById("Result");
- searchResult.style.display="none";
+let main = document.getElementById("main");
+ 
+ 
 
  let id, title, author, description, bookImg, bookMark
-const btnAdd = document.getElementById("btn1");
-const btnCancel = document.getElementById("btn2");
-const btnSearch = document.getElementById("btn3");
+
+
+
+
+
+main.innerHTML= `<button input type="button" id="btn1" onclik= "function addBooks()"   class=".btn" style="text-align: center">Ajouter un livre</button>`
+main.innerHTML += `<div id="div1"></br>
+Titre du livre <input id="champTitre" type="text" value=""></br>
+</br>
+Auteur du Livre <input id="champAuteur" type="text" value=""></br>
+</br>
+<button inputType="Search" id="btn3" style="text-align:center">Rechercher</button>
+<button input type="reset" id="btn2" style="text-align: center">Annuler</button>
+</div></br>`
+main.innerHTML += `<div class="bookList">
+<h2 id= "Result" class="text-center">Résultat de recherche</h2>
+<div id="displayBook">
+</div>
+</div>`
+let add = document.getElementById("btn1");
+let divHide = document.getElementById("div1");
+divHide.style.display="none";
+let searchResult = document.getElementById("Result");
+searchResult.style.display = "none";
+let display = document.getElementById("displayBook");
+display.style.display= "none";
+
+
+
+
 
 function addBooks(){
     
-    if (document.getElementById("div1").style.display == 'none')
-    {
-         document.getElementById("div1").style.display = 'block';
+    if(divHide.style.display == 'none'){
+         divHide.style.display = 'block';
     }
+    
 }
 
-btnAdd.addEventListener("click", addBooks);
+add.addEventListener("click", addBooks);
 
 
+
+let btnCancel = document.getElementById("btn2");
 function cancelSearch(){
     
-    let getTitle = document.getElementById("champTitre");
-    let getAuthor = document.getElementById("champAuteur");
-    if(getTitle.value != ""){
-        getTitle.value = "";
-    }
-    if(getAuthor.value != ""){
-        getAuthor.value = "";
-    }
-    if(document.getElementById("div1").style.display == 'block')
-    {
-        document.getElementById("div1").style.display = 'none';
-    }
-    window.history.back();
+    location.reload();
 }
 btnCancel.addEventListener("click", cancelSearch);
 
+
+const btnSearch = document.getElementById("btn3");
 function searchBook(){
 
     
@@ -49,7 +66,7 @@ function searchBook(){
     let id, title, author, description, bookImg, bookMark
     let searchData1;
     let searchData2;
-   // let keyAPI = "key=AIzaSyAq8xs1_8LCggwaDVqpMg2QqoMEinvf0Qk";
+   
     
     if(searchTitle.value == ""){
         alert("Veuillez renseigner un titre.");
@@ -81,36 +98,39 @@ function searchBook(){
                
             }
             
+            
 }
 btnSearch.addEventListener("click", searchBook, displayResults);
 
 
 function displayResults(data){
     console.log(data.items);
-    for(var i=0; i< 1; i+=10){
+    for(var i=1; i< 2; i+=10){
         item = data.items[i];
         title = item.volumeInfo.title;
         authors = item.volumeInfo.authors;
         description = item.volumeInfo.description;
-        bookImg = item.volumeInfo.imageLinks;
-        if(bookImg= null)
+        bookImg = item.volumeInfo.imageLinks.smallThumbnail;
+        if(bookImg == null || bookImg == "" )
         {
-            bookImg= "logo/unavailable.png";
+            document.createElement("img");
+            img.src = "logo/unavailable.png";
         }
+        else{
 
         
         formatOutput(bookImg, title, authors,description);
         if(searchResult.style.display == 'none')
     {
         searchResult.style.display = 'block';
-    }     
+    }   
+}  
     }
 
 function formatOutput(bookImg, title, authors, description, bookIsbn){
-    let display = document.getElementById("displayBook");
     let viewUrl = 'book.html?isbn='+bookIsbn;
     //let htmlCard= document.createElement("div");
-    for(i=0; i<2; i+=10){
+    for(i=0; i<2; i++){
     display.innerHTML =
     `<div class="col-lg-6">
     <div class="card" style="">
