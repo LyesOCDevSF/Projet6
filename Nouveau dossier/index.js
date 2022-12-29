085e7e87713121b62c4938f9dcb58de5190808c9
LyesOCDevSF <fourcroy.l@outlook.fr>
2022-12-29 16:30:30 +0100

    ajout session storage

--- Nouveau dossier/index.js
+++ Nouveau dossier/index.js
@@ -1,8 +1,6 @@
 
 
 let main = document.getElementById("main");
- 
- // Préparation insertion du formulaire dans le html
 let imgsrc = "D:/P6/Nouveau dossier/logo/unavailable.png"
 let bookmark = "D:/P6/Nouveau dossier/logo/signet (2).png"
  let id, title, author, description, bookImg, bookMark
@@ -24,29 +22,14 @@ Auteur du Livre <input id="champAuteur" type="text" value=""></br>
 <h2 id= "Result" class="text-center">Résultat de recherche</h2>
 <div id="displayBook">
 </div>
-<div id="list-output" class="">
-<div class="row">
-</div>
-</div>
 </div>`
-/*main.innerHTML += `<div id="div1"></br>
-Titre du livre <input id="champTitre" type="text" value=""></br>
-</br>
-Auteur du Livre <input id="champAuteur" type="text" value=""></br>
-</br>
-<button inputType="Search" id="btn3" style="text-align:center">Rechercher</button>
-<button input type="reset" id="btn2" style="text-align: center">Annuler</button>
-</div></br>`
-main.innerHTML += `<div class="bookList">
-<h2 id= "Result" class="text-center">Résultat de recherche</h2>
-<div id="displayBook">
-</div>
-`
-main.innerHTML += `<div id="list-output" class="">
+
+/*<div id="list-output" class="">
 <div class="row">
 </div>
-</div>
-</div>`*/
+</div>*/
+
+
 let add = document.getElementById("btn1");
 let divHide = document.getElementById("div1");
 divHide.style.display="none";
@@ -118,53 +101,36 @@ function searchBook(){
                     return response.json();
                 })
                 .then(function(data){
-                    displayResults(data);
-                    //displayResults(outputList);
-                })
-               
+                    console.log(data);
+                    data.items.forEach(book =>{
+                    displayResults(book);})})
+                    }  
             }
-            
-            
-}
+
 btnSearch.addEventListener("click", searchBook);
 
 
 // affichage des resultats de la recherche 
-let outputList = document.getElementById("list-output");
+//let outputList = document.getElementById("list-output");
 
-    outputList.style.display="none";
-function displayResults(data){
-    //outputList.appendChild(display);
-    console.log(data.items);
+function displayResults(book){
+    //console.log(data.items);
+        //item = book.items[i];
+       
+        let outputList = document.createElement('section');
+        outputList.className = 'outputlist';
+        outputList.setAttribute("id", book.id);
+            outputList.style.display="none";
+
+        title = book.volumeInfo.title;
+        authors = book.volumeInfo.authors;
+        description =(book.volumeInfo.description).substring(0, 200);
+        bookImg = (book.volumeInfo.imageLinks) ? book.volumeInfo.imageLinks.smallThumbnail : imgsrc;
+        id = book.id;
     
-        item = data.items[0];
-        title = item.volumeInfo.title;
-        authors = item.volumeInfo.authors;
-        description = (item.volumeInfo.description).substring(0, 200);
-        bookImg = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.smallThumbnail : imgsrc
-        id = item.id;
+        outputList.innerHTML = formatOutput(bookImg, title, authors,description, id);
+        display.appendChild(outputList);
         
-        item2 = data.items[1];
-        title2 = item2.volumeInfo.title;
-        authors2 =  item2.volumeInfo.authors;
-        description2 = item2.volumeInfo.description.substring(0, 200);
-        bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.smallThumbnail : imgsrc
-        id2 = item2.id;
-
-        item3 = data.items[2];
-        title3 = item3.volumeInfo.title;
-        authors3 = item3.volumeInfo.authors
-        description3 =  item3.volumeInfo.description.substring(0, 200);
-        bookImg3  = (item3.volumeInfo.imageLinks) ? item3.volumeInfo.imageLinks.smallThumbnail : imgsrc
-        id3 = item3.id;
-        
-        outputList.innerHTML += '<div class="row mt-4">' +
-        `<table><tbody><tr>`+
-        formatOutput(bookImg, title, authors,description, id) +
-        formatOutput(bookImg2, title2, authors2, description2, id2) +
-        formatOutput(bookImg3, title3, authors3, description3, id3) +
-        `</tr></tbody></table>`+
-        `</div>`;
         if(searchResult.style.display == 'none')
     {
         searchResult.style.display = 'block';
@@ -172,43 +138,61 @@ function displayResults(data){
     if(outputList.style.display == 'none'){
         outputList.style.display = 'block';
     }
+
+        }
+    
+
  
     
 // format resultat de recherche 
 
-function formatOutput(bookImg, title, authors, description, id, bookIsbn){
-    let viewUrl = 'book.html?isbn='+bookIsbn;
-    //let htmlCard= document.createElement("div");
+function formatOutput(bookImg, title, authors, description, id){
     
         if(display.style.display = "none"){
             display.style.display= "block";
         }
         
-    //display.innerHTML =
-    var a =
-       `<td>
-       
-    <div class="card" style="">
-          <img src="${bookImg}" class="card-img" alt="...">
-          <button inputType="button" id="saveB" style="text-align: right"><img src="${bookmark}" classe= "bookmark"></button>
-        </div>
-          <div class="card-body">
-            <h5 class="card-title">${title}</h5>
-            <p class="card-text">Author: ${authors}</p>
-            <p class="card-text" maxLenght="200">Description: ${description}</p>
-            <p class="card-text">Id: ${id}</p>
-            <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
-  </div>
-  </td>`
     
+    var a =
+  `<header>
+          <input type="button" id="saveB" onclick="function myList()" style="text-align: right"><img src="${bookmark}" classe= "bookmark"></input>
+  <div class="title"><h3>${title}</h3></div>
+  </header>
+  <div class="authors"><h4>${authors}</h4></div>
+  <div class="idBook"><p>ID : ${id}<p></div>
+  <div class="description"><p>${description}...</p></div>
+  <div class="imgBook"><img src="${bookImg}"></div>
+  `;
    return a;
 }
 
+
+
+
 let saveBook = document.getElementById("saveB");
 
-function myList(){
+function myList(favorite){
     
-}
+    if (sessionStorage.getItem(favorite)) {
+        alert('Vous ne pouvez ajouter deux fois le même livre')
+} else {
 
+    let myBook = document.createElement('section');
+    myBook.className = 'favoriteBook';
+    myBook.setAttribute("id", favorite);
+    myBook.getElementById(favorite);
+    let outputList = document.getElementById(favorite);
+    let list = document.getElementById("h2");
+
+    myBook = outputList.cloneNode(true);
+    list.appendChild(myBook);
+    sessionStorage.setItem(favorite, myBook.innerHTML);
 }
+   
+}
+saveBook.addEventListener("click", myList);
+
+
+
+
 
