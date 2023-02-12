a7bb1e405644658bfcd8eba417fc8b93f95d5e74
LyesOCDevSF <fourcroy.l@outlook.fr>
2023-02-12 01:42:27 +0100

    clean code

--- index.js
+++ index.js
@@ -1,233 +1,264 @@
-
-
 let main = document.getElementById("main");
 let imgsrc = "D:/P6/unavailable.png"
 let bookmark = `<i class="fa-solid fa-bookmark"></i>`
 let id, title, author, description, bookImg, bookMark
 
 
+//##################################### Affichage du bouton ajouter ######################################### 
+const myBooks = document.getElementById("myBooks");
+let line = document.querySelector("hr");
+line.id = "line";
+
+let newBooks = document.createElement("div");
+newBooks.id = "Add";
+myBooks.appendChild(newBooks);
+newBooks = myBooks.insertBefore(newBooks, line);
+
+const btnAdd = document.createElement("button");
+const btnLabel = document.createTextNode("Ajouter un livre");
+newBooks.appendChild(btnAdd);
+btnAdd.appendChild(btnLabel);
+
+//###################################################### Ajout du formulaire ###################################
+let formContainer = document.createElement("div")
+formContainer.id = "formContainer";
+newBooks.appendChild(formContainer);
+let form = document.createElement("form");
+form.id = "serachForm";
+/*form.action = "/submit";
+form.method = "post";*/
+formContainer.appendChild(form);
+formContainer.style.display = "none";
+
+// ajout des champs de recherche //
+let inputTitle = document.createElement("input");
+inputTitle.type = "text";
+inputTitle.name = "Titre";
+inputTitle.placeholder = "titre";
+form.appendChild(inputTitle);
+
+let inputAuthor = document.createElement("input");
+inputAuthor.type = "text";
+inputAuthor.name = "Auteur";
+form.appendChild(inputAuthor);
+
+// ajout des boutons Rechercher et Annuler //
+
+let btnSearch = document.createElement("button");
+const btnLabelSearch = document.createTextNode("Rechercher");
+formContainer.appendChild(btnSearch);
+btnSearch.appendChild(btnLabelSearch);
+
+let br = document.createElement("br")
+formContainer.appendChild(br);
+
+let btnCancel = document.createElement("button");
+const btnLabelCancel = document.createTextNode("Annuler");
+formContainer.appendChild(btnCancel);
+btnCancel.appendChild(btnLabelCancel);
+
+//################# affichage des resultats de recherche #################################
 
 
 
-main.innerHTML= `<button input type="button" id="btnAdd" onclick= "function addBooks()"   class=".btn" style="text-align: center; display: inline">Ajouter un livre</button>
-<div id="div1"></br>
-<label class="labelTitre" style:"text-align: inherit">Titre du livre </label><input id="champTitre" type="text" value=""></br>
-</br>
-<label class="labelAuteur" style:"text-align: inherit">Auteur du Livre</label><input id="champAuteur" type="text" value=""></br>
-</br>
-<div id="div2">
-<button inputType="Search" onclick= "function searchBook()" id="btnSearch" style="text-align:center">Rechercher</button>
-</br>
-<button input type="reset" id="btnCancel" style="text-align: center">Annuler</button>
-</div>
-</div></br>
-<div class="bookList">
-<h2 id= "Result" class="text-center">Résultat de recherche</h2>
-<div id="displayBook">
-</div>
-</div>`
+const bookList = document.createElement("div");
+bookList.className = "bookList";
+newBooks.appendChild(bookList);
+const searchResult = document.createElement("h2");
+const searchResultTitle = document.createTextNode("Résultat de recherche");
+searchResult.appendChild(searchResultTitle);
+bookList.appendChild(searchResult);
+
+const displayBook = document.createElement("div");
+displayBook.id = "displayBook";
+bookList.appendChild(displayBook);
 
 
-
-
-let add = document.getElementById("btnAdd");
-let divHide = document.getElementById("div1");
-divHide.style.display="none";
-let searchResult = document.getElementById("Result");
 searchResult.style.display = "none";
-let display = document.getElementById("displayBook");
-display.style.display= "none";
+displayBook.style.display = "none";
 
 
 
 
+//########################## affichage formulaire dynamique ########################
 
-// bouton ajouter un livre : Affichage des champs de recherche
-function addBooks(){
-    
-    if(divHide.style.display == 'none'){
-         divHide.style.display = 'block';
+
+
+function addBooks() {
+
+    if (formContainer.style.display === 'none') {
+        formContainer.style.display = 'block';
     }
-    
+
 }
+btnAdd.addEventListener("click", addBooks);
 
-add.addEventListener("click", addBooks);
+//######################################## annulation de la recherche #####################################
 
+function cancelSearch() {
 
-// bouton annuler la recherche : rafraichissement de la page 
-let btnCancel = document.getElementById("btnCancel");
-function cancelSearch(){
-    
     location.reload();
 }
 btnCancel.addEventListener("click", cancelSearch);
 
-// bouton recherche : recherche le livre demandé 
-const btnSearch = document.getElementById("btnSearch");
+//############################################ Recherche des livres #######################################
 
-function searchBook(){
+function searchBook() {
 
-    
     let bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-    let searchTitle = document.getElementById("champTitre");
-    let searchAuthor = document.getElementById("champAuteur");
     let searchData1;
     let searchData2;
 
-    
-    if(searchTitle.value == "" ){
-        alert("Veuillez renseigner un titre.");
-        searchTitle.focus();
-        return false;
-    }
-    if(searchAuthor.value == ""){
-        alert("Veuillez renseigner un nom d'auteur");
-        searchAuthor.focus();
-        return false;
-    }
-        searchData1 = searchTitle.value;
-        searchData2 = searchAuthor.value;
-        bookUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchData1 +" " + searchData2;
-        if(searchData1 == "" || searchData1 == null ){
-            displayError();
-        }
-        if(searchData2 == "" || searchData2 == null){
-            displayError();
-        }
-            else{
-                fetch(bookUrl)
-               .then(function(response){
-                    console.log(response);
-                   return response.json();
-                })
-                .then(function(data){
-                    console.log(data);
-                    if (data.totalItems === 0) {
 
-                        alert("Aucun Résultat");
-                    }
-                    data.items.forEach(book =>{
-                    displayResults(book);})
-                })
-               
-                     
+    if (inputTitle.value == "") {
+        alert("Veuillez renseigner un titre.");
+        inputTitle.focus();
+        return false;
+    }
+    if (inputAuthor.value == "") {
+        alert("Veuillez renseigner un nom d'auteur");
+        inputAuthor.focus();
+        return false;
+    }
+    searchData1 = inputTitle.value;
+    searchData2 = inputAuthor.value;
+    bookUrl = "https://www.googleapis.com/books/v1/volumes?q=" + searchData1 + " " + searchData2;
+    if (searchData1 == "" || searchData1 == null) {
+        displayError();
+    }
+    if (searchData2 == "" || searchData2 == null) {
+        displayError();
+    }
+    else {
+        fetch(bookUrl)
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                if (data.totalItems === 0) {
+
+                    alert("Aucun Résultat");
                 }
-            }
-        
-    
-            
+                data.items.forEach(book => {
+                    displayResults(book);
+                })
+            })
+
+
+    }
+}
+
 
 btnSearch.addEventListener("click", searchBook);
 
+// #####################################affichage des resultats de la recherche############################### 
 
-// affichage des resultats de la recherche 
 
+function displayResults(book) {
 
-function displayResults(book){
-           
-        let outputList = document.createElement('section');
-        //outputList.style.gridColumn = "span 1";
-        outputList.className = 'outputlist';
-        outputList.setAttribute("id", book.id);
-        outputList.style.display="none";
+    let outputList = document.createElement('section');
+    outputList.name = "favoris";
+    outputList.className = 'outputlist';
+    outputList.setAttribute("id", book.id);
+    outputList.style.display = "none";
 
-        title = book.volumeInfo.title;
-        
-        authors = book.volumeInfo.authors && book.volumeInfo.authors.length ? book.volumeInfo.authors[0] : "inconnu";
-        if(book && book.volumeInfo && book.volumeInfo.description){
-            description =(book.volumeInfo.description).substring(0, 200);
-        }
-        
-        bookImg = (book.volumeInfo.imageLinks) ? book.volumeInfo.imageLinks.smallThumbnail : imgsrc;
-        id = book.id;
-    
-        outputList.innerHTML = formatOutput(bookImg, title, authors,description, id);
-        display.appendChild(outputList);
-        
-        if(searchResult.style.display == 'none')
-    {
+    title = book.volumeInfo.title;
+
+    authors = book.volumeInfo.authors && book.volumeInfo.authors.length ? book.volumeInfo.authors[0] : "inconnu";
+    if (book && book.volumeInfo && book.volumeInfo.description) {
+        description = (book.volumeInfo.description).substring(0, 200);
+    }
+
+    bookImg = (book.volumeInfo.imageLinks) ? book.volumeInfo.imageLinks.smallThumbnail : imgsrc;
+    id = book.id;
+
+    outputList.innerHTML = formatOutput(bookImg, title, authors, description, id);
+    displayBook.appendChild(outputList);
+
+    if (searchResult.style.display === 'none') {
         searchResult.style.display = 'block';
-    }   
-    if(outputList.style.display == 'none'){
+    }
+    if (outputList.style.display === 'none') {
         outputList.style.display = 'block';
     }
 
-        }
-    
+}
 
- 
-    
-// format resultat de recherche 
+function formatOutput(bookImg, title, authors, description, id) {
 
-
-function formatOutput(bookImg, title, authors, description, id){
-    
-    if(display.style.display === "none"){
-        display.style.display= "grid";
+    if (displayBook.style.display === "none") {
+        displayBook.style.display = "grid";
     }
     var a =
-  `<header>
-    <div class="bookMark"><i class="fa-solid fa-bookmark" onclick = myList('${id}')></i></div>
-  <div class="title"><h3>${title}</h3></div>
-  </header>
-  <div class="authors"><h4>${authors}</h4></div>
-  <div class="idBook"><p>ID : ${id}<p></div>
-  <div class="description"><p>${description}...</p></div>
-  <div class="imgBook"><img src="${bookImg}"></div>
-  `;
-   return a;
+        `<header>
+        <div class="bookMark"><i class="fa-solid fa-bookmark" onclick = myList('${id}')></i></div>
+      <div class="title"><h3>${title}</h3></div>
+      </header>
+      <div class="authors"><h4>${authors}</h4></div>
+      <div class="idBook"><p>ID : ${id}<p></div>
+      <div class="description"><p>${description}...</p></div>
+      <div class="imgBook"><img src="${bookImg}"></div>
+      `;
+    return a;
 }
 
 
 
 
-//#################################### affichage du livre enregistré dans ma poch'list########################################
 
+function myList(favorite) {
 
-function myList(favorite){
-    
     if (sessionStorage.getItem(favorite)) {
         alert('Vous ne pouvez ajouter deux fois le même livre')
-        }
-         else {
+    }
+    else {
 
+        const content = document.getElementById("content");
+        let saveTitle = document.getElementById("h2");
 
-   
-    let myBook = document.createElement('section');
-    myBook.className = 'favoriteBook';
-    myBook.setAttribute("id", favorite);
+        let save = document.createElement('div');
+        save.id = "save";
+        content.appendChild(save);
 
-    let bookCard = document.getElementById(favorite).cloneNode(true);
-    myBook.appendChild(bookCard);
+        let saveBook = document.createElement('section');
+        saveBook.name = "favoris"
+        saveBook.className = 'favoriteBook';
+        saveBook.setAttribute("id", favorite);
 
-    let save = document.getElementById("save");
-    save.appendChild(myBook);
+        let bookCard = document.getElementById(favorite).cloneNode(true);
+        saveBook.appendChild(bookCard);
 
-    // remplacement de l'icone marquepage avec l'icone corbeille //
-    let bookMark = myBook.querySelector('.bookMark');
-    let bookTrash = document.createElement('div');
-    bookTrash.className = 'bookTrash';
-    bookTrash.innerHTML = `<i class="fa-solid fa-trash" onclick="deleteBook('${favorite}')" ></i>`;
-    bookMark.replaceWith(bookTrash);
+        //let save = document.getElementById("save");
+        save.appendChild(saveBook);
 
-    sessionStorage.setItem(favorite, myBook.innerHTML);
+        // remplacement de l'icone marquepage avec l'icone corbeille //
+        let bookMark = saveBook.querySelector('.bookMark');
+        let bookTrash = document.createElement('div');
+        bookTrash.className = 'bookTrash';
+        bookTrash.innerHTML = `<i class="fa-solid fa-trash" onclick="deleteBook('${favorite}')" ></i>`;
+        bookMark.replaceWith(bookTrash);
 
-// supression du livre dans ma poche liste //
-    bookTrash.addEventListener('click', function deleteBook() {
-    myBook.parentElement.removeChild(myBook);
-    sessionStorage.removeItem(favorite);});
+        sessionStorage.setItem(favorite, saveBook.innerHTML);
 
+        // supression du livre dans ma poche liste //
+        bookTrash.addEventListener('click', function deleteBook() {
+            saveBook.parentElement.removeChild(myBook);
+            sessionStorage.removeItem(favorite);
+        });
+    }
+    
 }
-   
-}
+
 // function de supression du livre dans la liste //
-function deleteBook(favorite){
+function deleteBook(favorite) {
 
-    let myBook = document.getElementById(favorite);
+    let saveBook = document.getElementById(favorite);
     let save = document.getElementById('save');
-    save.removeChild(myBook);
-    
-    
+    save.removeChild(saveBook);
+
+
     sessionStorage.removeItem(favorite);
 }
 
@@ -235,39 +266,23 @@ function deleteBook(favorite){
 
 window.onload = function () {
 
-    let list = document.getElementById("list");
     
+    let save = document.createElement('div');
+    save.id = "save";
+    content.appendChild(save);
 
     for (let i = 0; i < sessionStorage.length; i++) {
 
-            let value = sessionStorage.getItem(sessionStorage.key(i));
-            let favorite = sessionStorage.key(i);
-            let myBook = '';
-            
-           
+        let value = sessionStorage.getItem(sessionStorage.key(i));
+        let favorite = sessionStorage.key(i);
 
-            if (favorite != "IsThisFirstTime_Log_From_LiveServer") {
-                    let myBook = document.createElement('section');
-                    myBook.setAttribute("id", favorite);
-                    myBook.getElementsByClassName(favorite);
-                    myBook.innerHTML = value;
+        if (favorite != "IsThisFirstTime_Log_From_LiveServer") {
 
-                    let save = document.getElementById("save");
-                    
-                    save.appendChild(myBook);
+            save.innerHTML += value;
+        }
 
-                   
-            }
-            
-            console.log("onload:" + i + "/" + favorite);
-            
+        console.log("onload:" + i + "/" + favorite);
+
     }
-    
+
 }
-
-
-
-
-
-
-
